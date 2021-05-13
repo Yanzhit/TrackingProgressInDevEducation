f2f26e615cfd6eb626d2c60d416c6ee40cea71a7
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-13 13:03:00 +0300

    Добавьте файлы проекта.

---
+++ TrackingProgressInDevEducation.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TrackingProgressInDevEducation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
