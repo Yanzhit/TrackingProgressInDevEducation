3b125e0cd768c102de5b478fa0d8ea88550f5f48
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-22 12:53:03 +0300

    Update GetByIdStudents

--- TrackingProgressInDevEducation/DB/SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-﻿-- exec GetStudents -- получить всех студентов
--- exec AddNewStudent 'Имя', 'Фамилия', 0.00 -- Добавить нового студента
--- exec RemoveStudents -- Очистка таблицы студентов
--- exec GetByIdStudent 5 -- получить студента по id
