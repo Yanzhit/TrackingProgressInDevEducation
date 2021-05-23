f8b0bf1e695f6f3aa4998a0526db9127c29ca337
Alisa <alisa07.work@gmail.com>
2021-05-23 22:08:16 +0300

    GetAllCourses added

--- TrackingProgressInDevEducationDB/dbo/Stored Procedures/TableCourses/GetAllCourses.sql
+++ TrackingProgressInDevEducationDB/dbo/Stored Procedures/TableCourses/GetAllCourses.sql
@@ -1,6 +1,3 @@
-﻿--CREATE PROCEDURE [dbo].[GetAllCourses]
---	@param1 int = 0,
---	@param2 int
---AS
---	SELECT @param1, @param2
---RETURN 0
+﻿CREATE PROCEDURE [dbo].[GetAllCourses]
+AS
+select * from [Courses]
