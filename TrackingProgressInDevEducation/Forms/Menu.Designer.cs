18cf9e6d8e630eb8c64cde98b23d847ee6d4daf8
ZLoo <Kozlov.s.v.1992@gmail.com>
2021-05-23 13:59:08 +0300

    Add button for form Menu

--- TrackingProgressInDevEducation/Forms/Menu.Designer.cs
+++ TrackingProgressInDevEducation/Forms/Menu.Designer.cs
@@ -29,24 +29,123 @@ namespace TrackingProgressInDevEducation.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.CreateGroup = new System.Windows.Forms.Button();
+            this.ManagerGroups = new System.Windows.Forms.Button();
+            this.ManagerShedules = new System.Windows.Forms.Button();
+            this.ManagerStudents = new System.Windows.Forms.Button();
+            this.HomeWorks = new System.Windows.Forms.Button();
+            this.ManagerComments = new System.Windows.Forms.Button();
+            this.ManagerPayments = new System.Windows.Forms.Button();
+            this.ManagerTeams = new System.Windows.Forms.Button();
+            this.ManagerCourses = new System.Windows.Forms.Button();
+            this.ManagerLectures = new System.Windows.Forms.Button();
+            this.ManagerVisits = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // CreateGroup
+            // ManagerGroups
             // 
-            this.CreateGroup.Location = new System.Drawing.Point(349, 165);
-            this.CreateGroup.Name = "CreateGroup";
-            this.CreateGroup.Size = new System.Drawing.Size(89, 47);
-            this.CreateGroup.TabIndex = 0;
-            this.CreateGroup.Text = "Создать группу";
-            this.CreateGroup.UseVisualStyleBackColor = true;
+            this.ManagerGroups.Location = new System.Drawing.Point(126, 109);
+            this.ManagerGroups.Name = "ManagerGroups";
+            this.ManagerGroups.Size = new System.Drawing.Size(89, 47);
+            this.ManagerGroups.TabIndex = 0;
+            this.ManagerGroups.Text = "Менеджер групп";
+            this.ManagerGroups.UseVisualStyleBackColor = true;
+            // 
+            // ManagerShedules
+            // 
+            this.ManagerShedules.Location = new System.Drawing.Point(221, 33);
+            this.ManagerShedules.Name = "ManagerShedules";
+            this.ManagerShedules.Size = new System.Drawing.Size(89, 47);
+            this.ManagerShedules.TabIndex = 1;
+            this.ManagerShedules.Text = "Менеджер расписаний";
+            this.ManagerShedules.UseVisualStyleBackColor = true;
+            // 
+            // ManagerStudents
+            // 
+            this.ManagerStudents.Location = new System.Drawing.Point(31, 109);
+            this.ManagerStudents.Name = "ManagerStudents";
+            this.ManagerStudents.Size = new System.Drawing.Size(89, 47);
+            this.ManagerStudents.TabIndex = 2;
+            this.ManagerStudents.Text = "Менеджер студентов";
+            this.ManagerStudents.UseVisualStyleBackColor = true;
+            // 
+            // HomeWorks
+            // 
+            this.HomeWorks.Location = new System.Drawing.Point(126, 180);
+            this.HomeWorks.Name = "HomeWorks";
+            this.HomeWorks.Size = new System.Drawing.Size(89, 47);
+            this.HomeWorks.TabIndex = 3;
+            this.HomeWorks.Text = "Домашнее задание";
+            this.HomeWorks.UseVisualStyleBackColor = true;
+            // 
+            // ManagerComments
+            // 
+            this.ManagerComments.Location = new System.Drawing.Point(31, 180);
+            this.ManagerComments.Name = "ManagerComments";
+            this.ManagerComments.Size = new System.Drawing.Size(89, 47);
+            this.ManagerComments.TabIndex = 4;
+            this.ManagerComments.Text = "Менеджер комментариев";
+            this.ManagerComments.UseVisualStyleBackColor = true;
+            // 
+            // ManagerPayments
+            // 
+            this.ManagerPayments.Location = new System.Drawing.Point(126, 233);
+            this.ManagerPayments.Name = "ManagerPayments";
+            this.ManagerPayments.Size = new System.Drawing.Size(89, 47);
+            this.ManagerPayments.TabIndex = 5;
+            this.ManagerPayments.Text = "Менеджер платежей";
+            this.ManagerPayments.UseVisualStyleBackColor = true;
+            // 
+            // ManagerTeams
+            // 
+            this.ManagerTeams.Location = new System.Drawing.Point(221, 180);
+            this.ManagerTeams.Name = "ManagerTeams";
+            this.ManagerTeams.Size = new System.Drawing.Size(89, 47);
+            this.ManagerTeams.TabIndex = 6;
+            this.ManagerTeams.Text = "Менеджер команд";
+            this.ManagerTeams.UseVisualStyleBackColor = true;
+            // 
+            // ManagerCourses
+            // 
+            this.ManagerCourses.Location = new System.Drawing.Point(31, 33);
+            this.ManagerCourses.Name = "ManagerCourses";
+            this.ManagerCourses.Size = new System.Drawing.Size(89, 47);
+            this.ManagerCourses.TabIndex = 7;
+            this.ManagerCourses.Text = "Менеджер курсов";
+            this.ManagerCourses.UseVisualStyleBackColor = true;
+            // 
+            // ManagerLectures
+            // 
+            this.ManagerLectures.Location = new System.Drawing.Point(126, 33);
+            this.ManagerLectures.Name = "ManagerLectures";
+            this.ManagerLectures.Size = new System.Drawing.Size(89, 47);
+            this.ManagerLectures.TabIndex = 9;
+            this.ManagerLectures.Text = "Менеджер лекций";
+            this.ManagerLectures.UseVisualStyleBackColor = true;
+            // 
+            // ManagerVisits
+            // 
+            this.ManagerVisits.Location = new System.Drawing.Point(221, 109);
+            this.ManagerVisits.Name = "ManagerVisits";
+            this.ManagerVisits.Size = new System.Drawing.Size(89, 47);
+            this.ManagerVisits.TabIndex = 10;
+            this.ManagerVisits.Text = "Менеджер посещения";
+            this.ManagerVisits.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.CreateGroup);
+            this.ClientSize = new System.Drawing.Size(336, 299);
+            this.Controls.Add(this.ManagerVisits);
+            this.Controls.Add(this.ManagerLectures);
+            this.Controls.Add(this.ManagerCourses);
+            this.Controls.Add(this.ManagerTeams);
+            this.Controls.Add(this.ManagerPayments);
+            this.Controls.Add(this.ManagerComments);
+            this.Controls.Add(this.HomeWorks);
+            this.Controls.Add(this.ManagerStudents);
+            this.Controls.Add(this.ManagerShedules);
+            this.Controls.Add(this.ManagerGroups);
             this.Name = "Menu";
             this.Text = "Menu";
             this.ResumeLayout(false);
@@ -55,6 +154,15 @@ namespace TrackingProgressInDevEducation.Forms
 
         #endregion
 
-        private System.Windows.Forms.Button CreateGroup;
+        private System.Windows.Forms.Button ManagerGroups;
+        private System.Windows.Forms.Button ManagerShedules;
+        private System.Windows.Forms.Button ManagerStudents;
+        private System.Windows.Forms.Button HomeWorks;
+        private System.Windows.Forms.Button ManagerComments;
+        private System.Windows.Forms.Button ManagerPayments;
+        private System.Windows.Forms.Button ManagerTeams;
+        private System.Windows.Forms.Button ManagerCourses;
+        private System.Windows.Forms.Button ManagerLectures;
+        private System.Windows.Forms.Button ManagerVisits;
     }
 }
