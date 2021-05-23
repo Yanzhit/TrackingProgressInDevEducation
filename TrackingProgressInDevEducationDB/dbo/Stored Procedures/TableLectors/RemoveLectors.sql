48b93e6205a8a4233e9efdef03e504aaf0b5724a
Vadim <vadimsanduca@gmail.com>
2021-05-23 17:38:53 +0300

    CreateProcedursForTableLectors

---
+++ TrackingProgressInDevEducationDB/dbo/Stored Procedures/TableLectors/RemoveLectors.sql
@@ -0,0 +1,4 @@
+﻿CREATE PROCEDURE [dbo].[RemoveLectors]
+AS
+delete from [dbo].[Lectors]
+DBCC CHECKIDENT ([Lectors], RESEED, 0)  -- Обнуление ключа indentify
---
+++ TrackingProgressInDevEducationDB/dbo/Stored Procedures/TableLectors/UpdateLectorById.sql
@@ -0,0 +1,4 @@
+﻿CREATE PROCEDURE [dbo].[UpdateLectorById](@ID int, @FullName nvarchar(50))
+AS
+update [dbo].[Lectors] set FullName=@FullName
+where id=@ID
