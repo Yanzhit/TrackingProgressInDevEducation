06d246c512d53b06e58a44de3439cedf772cf798
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-24 02:02:04 +0300

    fix

--- TrackingProgressInDevEducation/Pages/SignIn.xaml.cs
+++ TrackingProgressInDevEducation/Pages/SignIn.xaml.cs
@@ -24,5 +24,10 @@ namespace TrackingProgressInDevEducation.Pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
