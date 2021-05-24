334da38f13373caf87f4eabeaeff0b66ad420e8e
Vadim <vadimsanduca@gmail.com>
2021-05-24 15:46:30 +0300

    WriteLector_Group

--- TrackingProgressInDevEducationDAL/Models/Bases/LectorGroup.cs
+++ TrackingProgressInDevEducationDAL/Models/Bases/LectorGroup.cs
@@ -1,6 +1,6 @@
 ﻿namespace TrackingProgressInDevEducationDAL.Models.Bases
 {
-    public class LectorGroup
+    public struct LectorGroup
     {
         public int LectorId { get; set; }
         public int GroupId { get; set; }
