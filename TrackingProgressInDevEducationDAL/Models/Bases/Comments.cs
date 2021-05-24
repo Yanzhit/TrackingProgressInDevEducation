6ee290d9e78785a353cb13855637623b0fd79885
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-25 01:19:26 +0300

    Write Comments

--- TrackingProgressInDevEducationDAL/Models/Bases/Comments.cs
+++ TrackingProgressInDevEducationDAL/Models/Bases/Comments.cs
@@ -2,6 +2,9 @@
 {
     public class Comments
     {
-        
+        public string Text { get; set; }
+        public int TypeId { get; set; }
+        public int StudentId { get; set; }
+        public int CreatedBy { get; set; }
     }
 }
