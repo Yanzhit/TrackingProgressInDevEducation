1b351c4f64ce72e353e1db75cd7ae08801fb37e5
Vadim <vadimsanduca@gmail.com>
2021-05-24 15:38:02 +0300

    WriteLector_Group

--- TrackingProgressInDevEducationDAL/Models/Bases/LectorGroup.cs
+++ TrackingProgressInDevEducationDAL/Models/Bases/LectorGroup.cs
@@ -2,6 +2,12 @@
 {
     public class LectorGroup
     {
-        
+        public int LectorId { get; set; }
+        public int GroupId { get; set; }
+        //public LectorGroup()
+        //{
+        //    LectorId=
+        //    GroupId=
+        //}
     }
 }
