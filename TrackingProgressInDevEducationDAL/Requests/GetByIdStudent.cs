8abde793539dcac9177fd4924e46fd3754819743
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-26 04:14:56 +0300

    Add GetByIdStudent

---
+++ TrackingProgressInDevEducationDAL/Requests/GetByIdStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests
+{
+    public class GetByIdStudent : IQuery
+    {
+        public Type Type { get; }
+        public string QueryName { get; }
+
+        public GetByIdStudent()
+        {
+            Type = typeof(Students);
+            QueryName = "GetByIdStudent";
+        }
+    }
+}
