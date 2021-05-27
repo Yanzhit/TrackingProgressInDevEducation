0b39234e77675a3462944701e158891e99291ae3
Vadim <vadimsanduca@gmail.com>
2021-05-27 14:24:53 +0300

    FinishQueriestoCommentTypesTable

---
+++ TrackingProgressInDevEducationDAL/Requests/NoParams/GetAllCommentTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.NoParams
+{
+    public class GetAllCommentTypes : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public GetAllCommentTypes()
+        {
+            Type = typeof(CommentType);
+            Name = "GetAllCommentTypes";
+            Params = "";
+        }
+    }
+}
---
+++ TrackingProgressInDevEducationDAL/Requests/Params/DeleteSelectedCommentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.NoParams
+{
+    public class DeleteSelectedCommentType : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public DeleteSelectedCommentType(int id)
+        {
+            Type = typeof(CommentType);
+            Name = "DeleteSelectedCommentType";
+            Params = $"{id}";
+        }
+    }
+}
