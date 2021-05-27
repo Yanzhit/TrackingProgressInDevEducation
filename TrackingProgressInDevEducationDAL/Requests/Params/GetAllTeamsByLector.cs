b757b322086fcc1e24140398462ac4b6320b823d
Yanzhit <yanzhit@gmail.com>
2021-05-27 15:10:58 +0300

    add class for teams table

---
+++ TrackingProgressInDevEducationDAL/Requests/Params/GetAllTeamsByLector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+namespace TrackingProgressInDevEducationDAL.Requests.Params
+{
+    public class GetAllTeamsByLector : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public GetAllTeamsByLector(int LectorId)
+        {
+            Type = typeof(Teams);
+            Name = "GetAllTeamsByLector";
+            Params = $"{LectorId}";
+        }
+    }
+}
---
+++ TrackingProgressInDevEducationDAL/Requests/Params/GetTeamById.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Params
+{
+    public class GetTeamById : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public GetTeamById(int id)
+        {
+            Type = typeof(Teams);
+            Name = "GetTeamById";
+            Params = $"{id}";
+        }
+    }
+}
