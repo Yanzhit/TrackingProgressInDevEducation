ebee01fb8b12158b09e1f2a6a0ea8535772b835c
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:51:19 +0300

    Write RemoveGroupById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeRemoveGroupById
+    {
+        public List<Groups> RemoveGroupById(int id)
+        {
+            RemoveGroupById removeGroupById = new RemoveGroupById(id);
+            return (List<Groups>)QuerySettings.QuerySet(removeGroupById);
+        }
     }
 }
