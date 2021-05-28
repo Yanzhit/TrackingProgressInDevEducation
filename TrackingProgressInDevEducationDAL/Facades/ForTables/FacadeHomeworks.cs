a36dbb6772a615bd3d4cac5425234e64fe959918
Vadim <vadimsanduca@gmail.com>
2021-05-28 21:53:47 +0300

    Write CreateNewHomeworks

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeHomeworks.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeHomeworks.cs
@@ -1,7 +1,21 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeHomeworks
     {
-        
+
     }
+
+    public class FacadeCreateNewHomeworks
+    {
+        public List<Homeworks> CreateNewHomeworks(string name)
+        {
+            CreateNewHomeworks createNewHomeworks = new CreateNewHomeworks(name);
+            return (List<Homeworks>)QuerySettings.QuerySet(createNewHomeworks);
+        }
+    }
+
 }
