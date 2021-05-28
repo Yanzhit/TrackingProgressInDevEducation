494a51e234a9e04f0f0b0278c96b6b2dd1d659ff
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:37:16 +0300

    Fix NullifyStudents

--- TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyStudents.cs
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyStudents.cs
@@ -14,7 +14,7 @@ namespace TrackingProgressInDevEducationDAL.Requests.Nullify
         {
             Type = typeof(Students);
             Name = GetType().Name;
-            Params = "";
+            Params = string.Empty;
         }
     }
 }
