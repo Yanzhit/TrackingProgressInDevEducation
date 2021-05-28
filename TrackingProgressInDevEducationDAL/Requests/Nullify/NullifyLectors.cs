3560a933d0a1dc4f2daa3698c415d2a2f5ce0ff8
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:34:26 +0300

    Add NullifyLectors

---
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyLectors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Nullify
+{
+    public class NullifyLectors : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public NullifyLectors()
+        {
+            Type = typeof(NullifyLectors);
+            Name = GetType().Name;
+            Params = string.Empty;
+        }
+    }
+}
