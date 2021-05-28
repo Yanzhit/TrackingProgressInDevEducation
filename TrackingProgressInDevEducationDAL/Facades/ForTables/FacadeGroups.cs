1ad7f5bbd900407b8858e9ee600145fc287eb2b0
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:56:26 +0300

    Write UpdateGroupById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeUpdateGroupById
+    {
+        public List<Groups> UpdateGroupById(int id, string name, int courseId)
+        {
+            UpdateGroupById updateGroupById = new UpdateGroupById(id, name, courseId);
+            return (List<Groups>)QuerySettings.QuerySet(updateGroupById);
+        }
     }
 }
