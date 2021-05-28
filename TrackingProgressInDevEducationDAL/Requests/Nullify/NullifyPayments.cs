371d2617ecce352c7dc84168a15dc04e77620982
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:36:04 +0300

    Add NullifyPayments

---
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyPayments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Nullify
+{
+    public class NullifyPayments : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public NullifyPayments()
+        {
+            Type = typeof(NullifyPayments);
+            Name = GetType().Name;
+            Params = string.Empty;
+        }
+    }
+}
