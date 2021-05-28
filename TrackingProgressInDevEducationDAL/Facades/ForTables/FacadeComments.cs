4ec9d44ae75677ac03da113ecb3fd7af1fc80f93
Vadim <vadimsanduca@gmail.com>
2021-05-29 00:11:03 +0300

    Write UpdateCommentById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeComments
     {
         
+    }
+
+    public class FacadeUpdateCommentById
+    {
+        public List<Comments> UpdateCommentById(int id, string text)
+        {
+            UpdateCommentById updateCommentById = new UpdateCommentById(id, text);
+            return (List<Comments>)QuerySettings.QuerySet(updateCommentById);
+        }
     }
 }
