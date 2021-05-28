3f7437f0d634220b639eae9144c1c41adbcec3ed
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 17:19:55 +0300

    Write Facade GetByIdStudent

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
@@ -1,7 +1,19 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeStudents
     {
-        
+    }
+
+    public class FacadeGetByIdStudent
+    {
+        public List<Students> GetByIdStudent(int id)
+        {
+            GetByIdStudent getByIdStudent = new GetByIdStudent(id);
+            return (List<Students>) QuerySettings.QuerySet(getByIdStudent);
+        }
     }
 }
