bf46ea9835ae1eb54609d6c5858462d474088046
Vadim <vadimsanduca@gmail.com>
2021-05-28 17:45:18 +0300

    Write GetLectors

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.NoParams;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeLectors
     {
         
+    }
+
+    public class FacadeGetLectors
+    {
+        public List<Lectors> GetLectors()
+        {
+            GetLectors getLectors = new GetLectors();
+            return (List<Lectors>)QuerySettings.QuerySet(getLectors);
+        }
     }
 }
