939bd149cf1c7ad60199eadd68df561a0c9cd29e
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:43:51 +0300

    Write GetGroupsByLector

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeGetGroupsByLector
+    {
+        public List<Groups> GetGroupsByLector(int lectorId)
+        {
+            GetGroupsByLector getGroupsByLector = new GetGroupsByLector(lectorId);
+            return (List<Groups>)QuerySettings.QuerySet(getGroupsByLector);
+        }
     }
 }
