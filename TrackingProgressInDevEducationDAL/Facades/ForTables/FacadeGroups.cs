2947687d223296eb06a80a07ce7df7c8c9817028
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:21:41 +0300

    Write GetGroups

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.NoParams;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeGetGroups
+    {
+        public List<Groups> GetGroups()
+        {
+            GetGroups getGroups = new GetGroups();
+            return (List<Groups>)QuerySettings.QuerySet(getGroups);
+        }
     }
 }
