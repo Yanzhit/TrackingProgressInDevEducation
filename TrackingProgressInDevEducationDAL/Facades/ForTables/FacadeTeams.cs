5700cf4367432c9489fd7fcdbdecda95e4d08c79
Vadim <vadimsanduca@gmail.com>
2021-05-29 02:09:32 +0300

    Write GetTeamById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeTeams
     {
         
+    }
+
+    public class FacadeGetTeamById
+    {
+        public List<Teams> GetTeamById(int id)
+        {
+            GetTeamById getTeamById = new GetTeamById(id);
+            return (List<Teams>)QuerySettings.QuerySet(getTeamById);
+        }
     }
 }
