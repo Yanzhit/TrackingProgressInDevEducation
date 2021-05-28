2caabedb7fc2844b29211b727d7517fea5345e43
Vadim <vadimsanduca@gmail.com>
2021-05-28 17:59:00 +0300

    GetLectorById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeLectors
     {
         
+    }
+
+    public class FacadeGetLectorById
+    {
+        public List<Lectors> GetLectorById(int id)
+        {
+            GetLectorById getLectorById = new GetLectorById(id);
+            return (List<Lectors>) QuerySettings.QuerySet(getLectorById);
+        }
     }
 }
