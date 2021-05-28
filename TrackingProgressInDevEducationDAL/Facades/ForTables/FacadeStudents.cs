7721ba40d843f7d70340ce0a079417ce7c2b1268
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 17:24:24 +0300

    Fix Marge

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.NoParams;
 using TrackingProgressInDevEducationDAL.Requests.Params;
 
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeStudents
     {
@@ -12,7 +14,7 @@ using TrackingProgressInDevEducationDAL.Requests.Params;
         public List<Students> GetStudents()
         {
             GetStudents getStudents = new GetStudents();
-            return (List<Students>)QuerySettings.QuerySet(getStudents);
+            return (List<Students>) QuerySettings.QuerySet(getStudents);
         }
     }
 
