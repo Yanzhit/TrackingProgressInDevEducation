e6be43cd25fdb2dd822ef44bd25d6f52c22d8493
Vadim <vadimsanduca@gmail.com>
2021-05-29 00:06:25 +0300

    Write Comments

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeComments
     {
         
+    }
+
+    public class FacadeRemoveCommentById
+    {
+        public List<Comments> RemoveCommentById(int id)
+        {
+            RemoveCommentById removeCommentById = new RemoveCommentById(id);
+            return (List<Comments>) QuerySettings.QuerySet(removeCommentById);
+        }
     }
 }
