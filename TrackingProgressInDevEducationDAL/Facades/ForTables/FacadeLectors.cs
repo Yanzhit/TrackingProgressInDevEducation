26e66220e448404546e503bc981aa1b35497df25
Vadim <vadimsanduca@gmail.com>
2021-05-28 18:03:36 +0300

    Write RemoveLectorById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeLectors
     {
         
+    }
+
+    public class FacadeRemoveLectorById
+    {
+        public List<Lectors> RemoveLectorById(int id)
+        {
+            RemoveLectorById removeLectorById = new RemoveLectorById(id);
+            return (List<Lectors>) QuerySettings.QuerySet(removeLectorById);
+        }
     }
 }
