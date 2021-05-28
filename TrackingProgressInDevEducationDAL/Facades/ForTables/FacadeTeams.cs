c991517055769c4b694749f53e07eaaa5b81a3b5
Vadim <vadimsanduca@gmail.com>
2021-05-29 02:26:46 +0300

    Write UpdateTeamById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
@@ -1,7 +1,21 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using System.Globalization;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeTeams
     {
         
+    }
+
+    public class FacadeUpdateTeamById
+    {
+        public List<Teams> UpdateTeamById(int id, string name)
+        {
+            UpdateTeamById updateTeamById = new UpdateTeamById(id, name);
+            return (List<Teams>) QuerySettings.QuerySet(updateTeamById);
+        }
     }
 }
