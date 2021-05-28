240213e56eecdffe4b9038d288378153c50c503c
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 17:17:29 +0300

    Write Facade AddNewStudent

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
@@ -1,7 +1,17 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeStudents
     {
-        
+    }
+
+    public class FacadeAddNewStudent
+    {
+        public string AddNewStudent(string name, string surname, decimal rate)
+        {
+            AddNewStudent addNewStudent = new AddNewStudent(name, surname, rate);
+            return (string) QuerySettings.QuerySet(addNewStudent);
+        }
     }
 }
