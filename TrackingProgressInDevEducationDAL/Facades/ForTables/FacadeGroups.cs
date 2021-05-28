3fc5003e8b461d6c1c60ebe12d55db135d008532
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:32:29 +0300

    Write AddNewGroup

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeAddNewGroup
+    {
+        public List<Groups> AddNewGroup(string name, int courseId)
+        {
+            AddNewGroup addNewGroup = new AddNewGroup(name, courseId);
+            return (List<Groups>)QuerySettings.QuerySet(addNewGroup);
+        }
     }
 }
