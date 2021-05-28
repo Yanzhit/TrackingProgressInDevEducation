ca454a5330e0aed8dcadab47e575354e45b0ed2d
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:16:40 +0300

    Add NullifyCommentType

---
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyCommentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Nullify
+{
+    public class NullifyCommentType : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public NullifyCommentType()
+        {
+            Type = typeof(NullifyCommentType);
+            Name = GetType().Name;
+            Params = string.Empty;
+        }
+    }
+}
