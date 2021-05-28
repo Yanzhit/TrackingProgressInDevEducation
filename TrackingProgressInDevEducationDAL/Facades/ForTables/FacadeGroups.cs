19561b2102aa267f2ea830f75fb0afaaa79697b3
Yanzhit <yanzhit@gmail.com>
2021-05-29 00:40:02 +0300

    Write GetGroupById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeGroups.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeGroups
     {
         
+    }
+
+    public class FacadeGetGroupById
+    {
+        public List<Groups> GetGroupById(int id)
+        {
+            GetGroupById getGroupById = new GetGroupById(id);
+            return (List<Groups>)QuerySettings.QuerySet(getGroupById);
+        }
     }
 }
