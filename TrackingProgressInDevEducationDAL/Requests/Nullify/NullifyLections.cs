343089053c831a5a59a12f1795efc5c7ee48e7ce
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:28:32 +0300

    Add NullifyLections

---
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyLections.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Nullify
+{
+    public class NullifyLections : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public NullifyLections()
+        {
+            Type = typeof(NullifyLections);
+            Name = GetType().Name;
+            Params = string.Empty;
+        }
+    }
+}
