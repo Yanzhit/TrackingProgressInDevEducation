e33ef7162016e4079c0990ea2f9a2c583f4916cf
Vadim <vadimsanduca@gmail.com>
2021-05-28 18:06:34 +0300

    Write UpdateLectorById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeLectors
     {
         
+    }
+
+    public class FacadeUpdateLectorById
+    {
+        public List<Lectors> UpdateLectorById(int id, string fullName)
+        {
+            UpdateLectorById updateLectorById = new UpdateLectorById(id, fullName);
+            return (List<Lectors>) QuerySettings.QuerySet(updateLectorById);
+        }
     }
 }
