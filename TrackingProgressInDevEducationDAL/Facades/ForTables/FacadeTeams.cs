866a57d7406baea936059c8995e01a767c4743c0
Vadim <vadimsanduca@gmail.com>
2021-05-29 02:19:46 +0300

    Write RemoveTeamById

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeTeams
     {
         
+    }
+
+    public class FacadeRemoveTeamById
+    {
+        public List<Teams> RemoveTeamById(int id)
+        {
+            RemoveTeamById removeTeamById = new RemoveTeamById(id);
+            return (List<Teams>) QuerySettings.QuerySet(removeTeamById);
+        }
     }
 }
