6b579d9607d5075561adc10591759d7fd502cc57
Vadim <vadimsanduca@gmail.com>
2021-05-28 21:24:49 +0300

    Write AddNewComment

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeComments
     {
         
+    }
+
+    public class FacadeAddNewComment
+    {
+        public List<Comments> AddNewComment(string text, int typeId, int studentId, int createdBy)
+        {
+            AddNewComment addNewComment = new AddNewComment(text, typeId, studentId, createdBy);
+            return (List<Comments>) QuerySettings.QuerySet(addNewComment);
+        }
     }
 }
