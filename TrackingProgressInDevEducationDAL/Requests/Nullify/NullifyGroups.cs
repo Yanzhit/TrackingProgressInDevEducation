abe22a6edc8c762b2bc4a267b2a6052dceb9d20c
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 13:18:21 +0300

    Add NullifyGroups

---
+++ TrackingProgressInDevEducationDAL/Requests/Nullify/NullifyGroups.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TrackingProgressInDevEducationDAL.Requests.Interface;
+
+namespace TrackingProgressInDevEducationDAL.Requests.Nullify
+{
+    public class NullifyGroups : IQuery
+    {
+        public Type Type { get; }
+        public string Name { get; }
+        public string Params { get; }
+
+        public NullifyGroups()
+        {
+            Type = typeof(NullifyGroups);
+            Name = GetType().Name;
+            Params = string.Empty;
+        }
+    }
+}
