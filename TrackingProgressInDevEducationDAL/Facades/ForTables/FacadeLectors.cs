e2de323f1581415f040e953798319537401c6e2a
Vadim <vadimsanduca@gmail.com>
2021-05-28 17:54:22 +0300

    Write AddNewLector

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeLectors.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeLectors
     {
         
+    }
+
+    public class FacadeAddNewLector
+    {
+        public List<Lectors> AddNewLector(string fullName)
+        {
+            AddNewLector addNewLector = new AddNewLector(fullName);
+            return (List<Lectors>)QuerySettings.QuerySet(addNewLector);
+        }
     }
 }
