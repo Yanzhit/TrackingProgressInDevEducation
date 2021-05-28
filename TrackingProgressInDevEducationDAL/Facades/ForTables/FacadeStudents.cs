9a04c4c0a78a2a52152d5844d6458775480c850c
Zombach <kozlov.s.v.1992@gmail.com>
2021-05-28 16:56:34 +0300

    Create GetStudents

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeStudents.cs
@@ -1,7 +1,19 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.NoParams;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeStudents
     {
-        
+    }
+
+    public class FacadeGetStudents
+    {
+        public List<Students> GetStudents()
+        {
+            GetStudents getStudents = new GetStudents();
+            return (List<Students>)QuerySettings.QuerySet(getStudents);
+        }
     }
 }
