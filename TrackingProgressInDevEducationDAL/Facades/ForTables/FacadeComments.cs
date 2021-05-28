732fbfe05258b164872d8ef16417674d56eca379
Vadim <vadimsanduca@gmail.com>
2021-05-28 21:30:45 +0300

    Write GetByIdComment

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeComments.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeComments
     {
         
+    }
+
+    public class FacadeGetByIdComment
+    {
+        public List<Comments> GetByIdComment(int id)
+        {
+            GetByIdComment getByIdComment = new GetByIdComment(id);
+            return (List<Comments>)QuerySettings.QuerySet(getByIdComment);
+        }
     }
 }
