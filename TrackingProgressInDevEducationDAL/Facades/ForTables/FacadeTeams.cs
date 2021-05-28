0e9990098f5ef3ee49a867d00eec8e41b6fc70cb
Vadim <vadimsanduca@gmail.com>
2021-05-29 02:03:24 +0300

    Write AddNewTeam

--- TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
+++ TrackingProgressInDevEducationDAL/Facades/ForTables/FacadeTeams.cs
@@ -1,7 +1,20 @@
-﻿namespace TrackingProgressInDevEducationDAL.Facades.ForTables
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Params;
+
+namespace TrackingProgressInDevEducationDAL.Facades.ForTables
 {
     public class FacadeTeams
     {
         
+    }
+
+    public class FacadeAddNewTeam
+    {
+        public List<Teams> AddNewTeam(string name)
+        {
+            AddNewTeam addNewTeam = new AddNewTeam(name);
+            return (List<Teams>) QuerySettings.QuerySet(addNewTeam);
+        }
     }
 }
