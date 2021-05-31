b1e14d0a41a4e1fe9c915c8da441bbc5d104f4a3
Wladimir <vv.krugliy@gmail.com>
2021-05-31 23:53:36 +0300

    Auto stash before merge of "SPForeStudentsPages" and "origin/Dev"

---
+++ TrackingProgressInDevEducationDB/TrackingProgressInDevEducationDB/Tables/CommentFor.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [TrackingProgressInDevEducationDB].[CommentFor]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL
+)
--- TrackingProgressInDevEducationDB/TrackingProgressInDevEducationDB/Tables/Comments.sql
+++ TrackingProgressInDevEducationDB/TrackingProgressInDevEducationDB/Tables/Comments.sql
@@ -5,10 +5,12 @@
     [StudentId] INT           NULL,
     [CreatedBy] INT           NOT NULL,
     [TeamId] INT NULL, 
+    [CommentFor] INT NOT NULL, 
     CONSTRAINT [PK_COMMENTS] PRIMARY KEY CLUSTERED ([id] ASC),
     CONSTRAINT [Comment_fk0] FOREIGN KEY ([TypeId]) REFERENCES [TrackingProgressInDevEducationDB].[CommentType] ([id]),
     CONSTRAINT [Comment_fk1] FOREIGN KEY ([StudentId]) REFERENCES [TrackingProgressInDevEducationDB].[Students] ([id]),
     CONSTRAINT [Comment_fk2] FOREIGN KEY ([CreatedBy]) REFERENCES [TrackingProgressInDevEducationDB].[Lectors] ([id]), 
-    CONSTRAINT [Comment_fk3] FOREIGN KEY ([TeamId]) REFERENCES [TrackingProgressInDevEducationDB].[Teams]([id])
+    CONSTRAINT [Comment_fk3] FOREIGN KEY ([TeamId]) REFERENCES [TrackingProgressInDevEducationDB].[Teams]([id]), 
+    CONSTRAINT [Comment_fk4] FOREIGN KEY ([CommentFor]) REFERENCES [TrackingProgressInDevEducationDB].[CommentFor]([Id])
 );
 
