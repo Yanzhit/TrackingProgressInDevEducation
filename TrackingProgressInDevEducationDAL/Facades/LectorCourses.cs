26c3e9a938b5b5b1c358c42a6986d6d0550d0d9c
Vadim <vadimsanduca@gmail.com>
2021-06-01 17:30:07 +0300

    ReworkLectorCourses

--- TrackingProgressInDevEducationDAL/Facades/LectorCourses.cs
+++ TrackingProgressInDevEducationDAL/Facades/LectorCourses.cs
@@ -1,9 +1,21 @@
-﻿using TrackingProgressInDevEducationDAL.Requests.ForTables;
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.ForTables;
 
 namespace TrackingProgressInDevEducationDAL.Facades
 {
     public class LectorCourses
     {
-        private static readonly QLection Query = new();
+        private readonly QLectorCourse _qLectorCourse = new();
+        private readonly QuerySettings _query = new();
+
+        /// <summary>
+        /// Обнуление таблицы Лекторы Курсы
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<LectorCourse> NullifyCourse()
+        {
+            return (IEnumerable<LectorCourse>)_query.QuerySet(_qLectorCourse.NullifyCourse());
+        }
     }
 }
--- TrackingProgressInDevEducationDAL/Facades/LectorGroups.cs
+++ TrackingProgressInDevEducationDAL/Facades/LectorGroups.cs
@@ -1,9 +1,21 @@
-﻿using TrackingProgressInDevEducationDAL.Requests.ForTables;
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Requests.ForTables;
 
 namespace TrackingProgressInDevEducationDAL.Facades
 {
     public class LectorGroups
     {
-        private static readonly QLectorGroup Query = new();
+        private readonly QLectorGroup _qLectorGroup = new();
+        private readonly QuerySettings _query = new();
+
+
+        /// <summary>
+        /// Обнуление таблицы ДЗ ГРУППЫ и ключа identity
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<QHomeworkGroup> NullifyLectorGroup()
+        {
+            return (IEnumerable<QHomeworkGroup>)_query.QuerySet(_qLectorGroup.NullifyLectorGroup());
+        }
     }
 }
