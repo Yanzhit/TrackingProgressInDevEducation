95c364082ed97d8cee248ea47b0920e40778eb40
Vadim <vadimsanduca@gmail.com>
2021-06-01 15:58:52 +0300

    ReworkFacades

--- TrackingProgressInDevEducationDAL/Facades/CommentTypes.cs
+++ TrackingProgressInDevEducationDAL/Facades/CommentTypes.cs
@@ -1,9 +1,31 @@
-﻿using TrackingProgressInDevEducationDAL.Models.Bases;
+﻿using System.Collections.Generic;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.ForTables;
 
 namespace TrackingProgressInDevEducationDAL.Facades
 {
     public class CommentTypes
     {
-        //private static readonly CommentType Query = new();
+        private readonly QCommentType _qCommentType = new();
+        private readonly QuerySettings _query = new();
+
+        /// <summary>
+        /// Создать новый тип комментария
+        /// </summary>
+        /// <param name="name">Имя типа</param>
+        /// <returns>Тип комментариев</returns>
+        public CommentType SetNewCType(string name)
+        {
+            return (CommentType)_query.QuerySet(_qCommentType.SetNewCType(name));
+        }
+
+        /// <summary>
+        /// Обнуление таблицы Типов комментариев и ключа identity
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<CommentType> NullifyCTypes()
+        {
+            return (IEnumerable<CommentType>)_query.QuerySet(_qCommentType.NullifyCTypes());
+        }
     }
 }
