d94511eb27309b8a5d8777cb92576ecb5e1c8383
Zombach <kozlov.s.v.1992@gmail.com>
2021-06-03 00:38:57 +0300

    Fix

--- TrackingProgressInDevEducationDAL.Tests/Tests/TCommentTypes.cs
+++ TrackingProgressInDevEducationDAL.Tests/Tests/TCommentTypes.cs
@@ -23,7 +23,7 @@ namespace TrackingProgressInDevEducationDAL.Tests.Tests
         public void SetNewCTypeTest(IQuery query, CType commentType, object expected)
         {
             PreparindDB(query.Type);
-            _manager.CommentTypes.SetNewCType(commentType.Name);
+            _manager.CTypes.SetNewCType(commentType.Name);
             //Assert.AreEqual(expected, actual);
         }
 
@@ -36,7 +36,7 @@ namespace TrackingProgressInDevEducationDAL.Tests.Tests
         public void NullifyCTypesTest(IQuery query, object expected)
         {
             PreparindDB(query.Type);
-            _manager.CommentTypes.NullifyCTypes();
+            _manager.CTypes.NullifyCTypes();
             //Assert.AreEqual(expected, actual);
         }
     }
