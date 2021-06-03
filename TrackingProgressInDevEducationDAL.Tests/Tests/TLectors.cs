588b4e3a90d09f217c9e7fe4429a17e15eee3d73
Alex <bichcov@mal.ru>
2021-06-03 15:54:19 +0300

    AddLectors

---
+++ TrackingProgressInDevEducationDAL.Tests/Tests/TLectors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interfaces;
+using TrackingProgressInDevEducationDAL.Tests.Abstract;
+using TrackingProgressInDevEducationDAL.Tests.Expected;
+
+namespace TrackingProgressInDevEducationDAL.Tests.Tests
+{
+    /// <summary>
+    /// Тестирование всех запросов работающих только с таблицей лекторов
+    /// </summary>
+    public class TLectors : ATTests
+    {
+        /// <summary>
+        /// Добавление новых лекторов в таблицу
+        /// </summary>
+        /// <param name="query">Запрос</param>
+        /// <param name="lector">Входящий лектор(Ганибал)</param>
+        /// <param name="expected">Ожидаемый результат</param>
+        [TestCaseSource(typeof(ELectors), nameof(ELectors.SetNewLector))]
+        public void SetNewLectorTest(IQuery query, Lector lector, object expected)
+        {
+            PreparindDB(query.Type);
+            FacadeManager.Lectors.SetNewLector(lector.FullName, lector.Email, lector.Password);
+            //Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест на обнуления таблицы лекторов
+        /// </summary>
+        /// <param name="query">Запрос</param>
+        /// <param name="expected">Ожидаемый результат</param>
+        [TestCaseSource(typeof(ELectors), nameof(ELectors.NullifyLectors))]
+        public void NullifyLectorsTest(IQuery query, object expected)
+        {
+            PreparindDB(query.Type);
+            FacadeManager.Lectors.NullifyLectors();
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
