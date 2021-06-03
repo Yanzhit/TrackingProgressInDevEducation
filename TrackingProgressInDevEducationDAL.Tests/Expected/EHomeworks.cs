2dda093389de34e1d4cc9b3ffee7b519d4c32f4a
Alex <bichcov@mal.ru>
2021-06-03 15:22:11 +0300

    AddTests
    Домашние работы
    лекции

---
+++ TrackingProgressInDevEducationDAL.Tests/Expected/EHomeworks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Tables;
+
+namespace TrackingProgressInDevEducationDAL.Tests.Expected
+{
+    /// <summary>
+    /// Ожидаемый результат для разных запросов
+    /// </summary>
+    class EHomeworks
+    {
+        /// <summary>
+        /// Добавить новую домашнюю работу
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable SetNewHW()
+        {
+            yield return new object[] { new QHomework(), new Homework("C# dz"), 1 };
+            yield return new object[] { new QHomework(), new Homework("Java dz"), 1 };
+        }
+        /// <summary>
+        /// Очистка таблицы домашних работ
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable NullifyHWs()
+        {
+            yield return new object[] { new QHomework(), 1 };
+            yield return new object[] { new QHomework(), 1 };
+        }
+    }
+}
---
+++ TrackingProgressInDevEducationDAL.Tests/Tests/THWGroups.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using TrackingProgressInDevEducationDAL.Models.Bases;
+using TrackingProgressInDevEducationDAL.Requests.Interfaces;
+using TrackingProgressInDevEducationDAL.Tests.Abstract;
+using TrackingProgressInDevEducationDAL.Tests.Expected;
+
+namespace TrackingProgressInDevEducationDAL.Tests.Tests
+{
+    /// <summary>
+    /// Тестирование всех запросов работающих только с таблицей Домашних работ группы
+    /// </summary>
+    public class THWGroups : ATTests
+    {
+        /// <summary>
+        /// Добавление новой  домашней работы группы в таблицу
+        /// </summary>
+        /// <param name="query">Запрос</param>
+        /// <param name="hWGroup">Входящяя домашняя работа группы</param>
+        /// <param name="expected">Ожидаемый результат</param>
+        [TestCaseSource(typeof(EHWGroups), nameof(EHWGroups.SetNewHWGroup))]
+        public void SetNewHWGroupTest(IQuery query, HWGroup hWGroup, object expected)
+        {
+            PreparindDB(query.Type);
+            FacadeManager.HWGroups.SetNewHWGroup(hWGroup.HomeworkId, hWGroup.GroupId);
+            //Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Тест на обнуления таблицы домашних работ группы
+        /// </summary>
+        /// <param name="query">Запрос</param>
+        /// <param name="expected">Ожидаемый результат</param>
+        [TestCaseSource(typeof(EHWGroups), nameof(EHWGroups.NullifyHWGroup))]
+        public void NullifyHWGroupTest(IQuery query, object expected)
+        {
+            PreparindDB(query.Type);
+            FacadeManager.HWGroups.NullifyHWGroup();
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
