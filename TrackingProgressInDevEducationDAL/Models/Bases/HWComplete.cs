73635e8029a6af7ef138f446adf88b0c0aed5d96
Vadim <vadimsanduca@gmail.com>
2021-06-03 14:03:05 +0300

    UpdateHWComplete

--- TrackingProgressInDevEducationDAL/Models/Bases/HWComplete.cs
+++ TrackingProgressInDevEducationDAL/Models/Bases/HWComplete.cs
@@ -7,16 +7,22 @@ namespace TrackingProgressInDevEducationDAL.Models.Bases
         public int HomeworkId { get; set; }
         public int StudentId { get; set; }
         public bool Status { get; set; }
+        public DateTime? ApproveDate { get; set; }
+        public int? Score { get; set; }
+        public DateTime? CreatedOn { get; set; }
 
         public HWComplete()
         {
         }
 
-        public HWComplete(int homeworkId, int studentId, bool status)
+        public HWComplete(int homeworkId, int studentId, bool status, DateTime? approveDate, int? score, DateTime? createdOn)
         {
             HomeworkId = homeworkId;
             StudentId = studentId;
             Status = status;
+            ApproveDate = approveDate;
+            Score = score;
+            CreatedOn = createdOn;
         }
 
         public override bool Equals(object obj)
@@ -28,11 +34,14 @@ namespace TrackingProgressInDevEducationDAL.Models.Bases
         {
             return HomeworkId == other.HomeworkId
                    && StudentId == other.StudentId
-                   && Status == other.Status;
+                   && Status == other.Status
+                   && ApproveDate == other.ApproveDate
+                   && Score == other.Score
+                   && CreatedOn == other.CreatedOn;
         }
         public override int GetHashCode()
         {
-            return HashCode.Combine(HomeworkId, StudentId, Status);
+            return HashCode.Combine(HomeworkId, StudentId, Status, ApproveDate, Score, CreatedOn);
         }
     }
 }
