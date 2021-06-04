4306dd936eb91acfeda3a58b8f1e483782525d28
Zombach <kozlov.s.v.1992@gmail.com>
2021-06-04 22:48:31 +0300

    Fix Resourse

--- TrackingProgressInDevEducationUI/Pages/SignIn.xaml.cs
+++ TrackingProgressInDevEducationUI/Pages/SignIn.xaml.cs
@@ -14,30 +14,29 @@ namespace TrackingProgressInDevEducationUI.Pages
     // ReSharper disable once RedundantExtendsListEntry
     public partial class SignIn : Page
     {
+        private readonly SingleContents _contents = SingleContents.GetContent();
         private readonly FacadeManager _manager = new();
         public SignIn()
         {
             InitializeComponent();
         }
 
-        private void Image()
-        {
-        }
-
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
             Lector lector = _manager.Lectors.GetLoginAndPassword(Login.Text, Password.Text);
-           // _mainForm.Content = home;
+            _contents.Home();
         }
 
         private void Registration_Click(object sender, RoutedEventArgs e)
         {
-            //_mainForm.Content = registration;
+            _contents.Registration();
         }
 
         private void Logo_Click(object sender, RoutedEventArgs e)
         {
-            
+            /*
+             * Сюда можно о программе добавить
+             */
         }
     }
 }
