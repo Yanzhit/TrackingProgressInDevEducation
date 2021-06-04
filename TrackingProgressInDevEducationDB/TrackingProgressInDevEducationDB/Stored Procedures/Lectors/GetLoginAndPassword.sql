a9a48c6678a9f684f07445edcd961005b864c926
Vladimir Krugliy <vv.krugliy@outlook.com>
2021-06-04 14:10:59 +0300

    GetLoginAndPassword

---
+++ TrackingProgressInDevEducationDB/TrackingProgressInDevEducationDB/Stored Procedures/Lectors/GetLoginAndPassword.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [TrackingProgressInDevEducationDB].[GetLoginAndPassword]
+	@Login nvarchar (50),
+	@Password nvarchar (50)
+AS
+	SELECT [id]
+    ,[FullName]
+    ,[Email] 
+    ,[Password]
+	FROM [TrackingProgressInDevEducationDB].Lectors
+	WHERE [Email] = @Login AND [Password] = @Password
